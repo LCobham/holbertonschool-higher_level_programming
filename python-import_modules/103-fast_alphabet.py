a20eb7228eec604b327a608d9d2566e46085e21a
LCobham <lucascobham@hotmail.com>
2023-01-18 08:27:14 -0800

    New attempt in tasks 101 and 103

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
-        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'
-        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'
-        'X', 'Y', 'Z']
-
-print(*alpha, sep = "")
+alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+         'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+print(*alpha, sep="")
