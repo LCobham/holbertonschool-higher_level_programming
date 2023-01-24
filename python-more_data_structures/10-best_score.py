bcb5dff67cf79818dc0a6f4ba728081ab41b5691
LCobham <lucascobham@hotmail.com>
2023-01-24 07:58:29 -0800

    Fixed bugs in tasks 10, 11 and 12

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     items = list(a_dictionary.items())
     items.sort(key=lambda x: x[1])
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,8 +8,10 @@ def roman_to_int(roman_string):
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     for i, let in enumerate(roman_string):
-        nxt_value_lower = values.get(let) >= values.get(roman_string[i + 1])
-        if i == len(roman_string) - 1 or nxt_value_lower:
+        nxt_val_lower = 0
+        if i < len(roman_string) - 1:
+            nxt_val_lower = values.get(let) >= values.get(roman_string[i + 1])
+        if i == len(roman_string) - 1 or nxt_val_lower:
             res += values.get(let)
         else:
             res -= values.get(let)
