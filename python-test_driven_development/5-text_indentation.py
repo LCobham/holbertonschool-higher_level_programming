f8d462bf3c938d1c366246aa9f40d58dae8cb4ef
LCobham <lucascobham@hotmail.com>
2023-02-01 08:04:10 -0800

    Created task 5 with tests. Fixed coding style in 3

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    This module defines a function that prints a text with two new lines
+    after the characters '.', '?' or ':'.
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 newlines after special chars: '.', '?', ':'."""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    newString = text.replace('. ', '.\n\n')
+    newString = newString.replace('? ', '?\n\n')
+    newString = newString.replace(': ', ':\n\n')
+    print(f"{newString}", end="")
