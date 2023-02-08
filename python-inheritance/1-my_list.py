c85f963b6ace005658c5063c23915a91d3cffbfd
LCobham <lucascobham@hotmail.com>
2023-02-08 11:11:49 -0800

    New fix in task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,5 +9,5 @@ class MyList(list):
     """New list class with additional functionality"""
 
     def print_sorted(self):
-        """Prints the list in sorted order and return new list"""
-        return print(sorted(self))
+        """Rturns the list sorted in ascending order"""
+        return sorted(self)
