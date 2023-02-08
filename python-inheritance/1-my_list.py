015980ff05a08efa49aa35fccb8489a73ec2ac8a
LCobham <lucascobham@hotmail.com>
2023-02-08 11:05:33 -0800

    Fixed bugs in task 1 and respective tests

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,4 +10,4 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the list in sorted order and return new list"""
-        return sorted(self)
+        return print(sorted(self))
