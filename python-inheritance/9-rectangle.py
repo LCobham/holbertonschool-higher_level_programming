f80982ac4a51b44b3030394786ca89b239059f0f
LCobham <lucascobham@hotmail.com>
2023-02-09 09:58:40 -0800

    Created tasks 9 and 10. Small fix in 8

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -15,7 +15,14 @@ class Rectangle(BaseGeometry):
             Checks if width & heigh are valid integers and assigns them
             as private instance attributes
         """
-        if super().integer_validator("width", width) is None:
+        if BaseGeometry.integer_validator(self, "width", width) is None:
             self.__width = width
-        if super().integer_validator("height", height) is None:
+        if BaseGeometry.integer_validator(self, "height", height) is None:
             self.__height = height
+
+    def area(self):
+        """Calculate area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
