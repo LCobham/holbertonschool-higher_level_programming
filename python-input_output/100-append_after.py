cd16a5276932e85dfda21f2f03486289d3e50e16
LCobham <lucascobham@hotmail.com>
2023-02-12 07:20:47 -0800

    Trying task 100 without importing any modules

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -5,15 +5,13 @@
 """
 
 
-import os
-
-
 def append_after(filename="", search_string="", new_string=""):
     """Append new_string after each occurrance of search_string"""
-    with open(filename, 'r', encoding="utf-8") as f:
-        with open(filename + '__COPY__', 'w', encoding='utf-8') as rf:
-            for line in f:
-                rf.write(line)
-                if search_string in line:
-                    rf.write(new_string)
-    os.rename(filename + '__COPY__', filename)
+    with open(filename, 'r+', encoding="utf-8") as f:
+        new_contents = ''
+        for line in f:
+            new_contents += line
+            if search_string in line:
+                new_contents += new_string
+        f.seek(0)
+        f.write(new_contents)
