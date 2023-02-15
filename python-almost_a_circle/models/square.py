5182202afeca9aafe98799d3062946b068f843ce
LCobham <lucascobham@hotmail.com>
2023-02-15 09:38:17 -0800

    Fixed minor bug in square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,12 +23,8 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be > 0")
-        self._Rectangle__width = value
-        self._Rectangle__height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """
