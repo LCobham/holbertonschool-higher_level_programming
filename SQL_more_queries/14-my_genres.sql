833c837cb957604c55f5e574a113fc9838c33694
LCobham <lucascobham@hotmail.com>
2023-03-15 08:27:04 -0700

    Createdtasks 14-16, fixed 8

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- List all the genres listed for 'Dexter'
+SELECT tv_genres.name
+    FROM tv_genres, tv_show_genres, tv_shows
+    WHERE tv_genres.id = tv_show_genres.genre_id AND
+        tv_shows.id = tv_show_genres.show_id AND
+        tv_shows.title = 'Dexter'
+    ORDER BY name;
--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- List all the cities in California in hbtn_0d_usa database
-SELECT * FROM cities
+SELECT id, name FROM cities
     WHERE state_id = (
         SELECT id FROM states
             WHERE name='California'
