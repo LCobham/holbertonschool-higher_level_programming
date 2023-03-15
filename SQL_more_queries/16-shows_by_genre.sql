5c395571ac0a282dc8d6080af072163d3a994324
LCobham <lucascobham@hotmail.com>
2023-03-15 08:33:27 -0700

    Fixed bug in 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_shows.title, tv_genres.name
     ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres
     ON tv_genres.id = tv_show_genres.genre_id
-    ORDER BY title;
+    ORDER BY title, name;
