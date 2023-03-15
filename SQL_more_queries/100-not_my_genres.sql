cdb025a3e0d9390325e68d14ec38654df283ea51
LCobham <lucascobham@hotmail.com>
2023-03-15 10:52:52 -0700

    Added advanced tasks 100-103

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,13 @@
+-- Select all genres which are not related to Dexter
+
+SELECT tv_genres.name 
+    FROM tv_genres
+    WHERE tv_genres.name NOT IN (
+        SELECT DISTINCT tv_genres.name
+            FROM tv_genres JOIN tv_show_genres
+            ON tv_genres.id = tv_show_genres.genre_id
+            LEFT JOIN tv_shows
+            ON tv_shows.id = tv_show_genres.show_id
+            WHERE tv_shows.title = 'Dexter'
+    )
+    ORDER BY name;
