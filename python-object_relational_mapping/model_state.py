c3bf86376e27b8854174dcd4568608c5d5846ad3
LCobham <lucascobham@hotmail.com>
2023-03-17 09:22:38 -0700

    Changes in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,10 +12,6 @@ Base = declarative_base()
 class State(Base):
     """State class to connect with SQL database"""
     __tablename__ = 'states'
-    id = Column("id", Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True)
     name = Column(String(128), nullable=False)
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
