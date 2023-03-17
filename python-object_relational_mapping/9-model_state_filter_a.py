d8c90d136fcecad1815aee66657cbbc74e1d3f8d
LCobham <lucascobham@hotmail.com>
2023-03-17 10:21:34 -0700

    Created task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+   List all states from the states table in hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+if __name__ == "__main__":
+    length = len(sys.argv)
+    if length < 4:
+        print("Incorrect number of args passed.")
+        print("USAGE: ./8-model_state_fetch_first.py " +
+              "username password database name")
+        sys.exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            sys.argv[1],
+                            sys.argv[2],
+                            sys.argv[3]
+    ))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(
+        State.name.like("%a%")).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
